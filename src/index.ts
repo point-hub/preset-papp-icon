cf3503bbb0f45e63009d069e143d26e22a4bbdf3
Martien Dermawan Tanama <martiendt@gmail.com>
2023-12-03 19:35:09 +0700

    chore: fix types

--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,72 @@
-import { presetIcons } from "unocss";
+import { type PresetFactory, presetIcons } from "unocss";
 
 export default function presetPappIcon(iconsUrl = "https://icons.iap.my.id") {
-  return {
-    name: "preset-papp-icon",
-    presets: [
-      presetIcons({
-        cdn: "https://esm.sh/",
-        extraProperties: {
-          display: "block",
-        },
-        collections: {
-          fab: async (iconName: string) =>
-            await fetch(
-              `${iconsUrl}/fontawesome/6/svgs/brands/${iconName}.svg`
-            ).then((res) => res.text()),
-          far: async (iconName: string) =>
-            await fetch(
-              `${iconsUrl}/fontawesome/6/svgs/regular/${iconName}.svg`
-            ).then((res) => res.text()),
-          fas: async (iconName: string) =>
-            await fetch(
-              `${iconsUrl}/fontawesome/6/svgs/solid/${iconName}.svg`
-            ).then((res) => res.text()),
-          fal: async (iconName: string) =>
-            await fetch(
-              `${iconsUrl}/fontawesome/6/svgs/light/${iconName}.svg`
-            ).then((res) => res.text()),
-          fat: async (iconName: string) =>
-            await fetch(
-              `${iconsUrl}/fontawesome/6/svgs/thin/${iconName}.svg`
-            ).then((res) => res.text()),
-          fad: async (iconName: string) =>
-            await fetch(
-              `${iconsUrl}/fontawesome/6/svgs/duotone/${iconName}.svg`
-            ).then((res) => res.text()),
-          fasr: async (iconName: string) =>
-            await fetch(
-              `${iconsUrl}/fontawesome/6/svgs/sharp-regular/${iconName}.svg`
-            ).then((res) => res.text()),
-          fass: async (iconName: string) =>
-            await fetch(
-              `${iconsUrl}/fontawesome/6/svgs/sharp-solid/${iconName}.svg`
-            ).then((res) => res.text()),
-          fasl: async (iconName: string) =>
-            await fetch(
-              `${iconsUrl}/fontawesome/6/svgs/sharp-light/${iconName}.svg`
-            ).then((res) => res.text()),
-        },
-        customizations: {
-          transform(svg, collection) {
-            if (
-              [
-                "fab",
-                "far",
-                "fas",
-                "fal",
-                "fat",
-                "fad",
-                "fasr",
-                "fass",
-                "fasl",
-              ].includes(collection)
-            )
-              return svg.replace("path", 'path fill="currentColor"');
+  return presetIcons({
+    cdn: "https://esm.sh/",
+    extraProperties: {
+      display: "block",
+    },
+    collections: {
+      fab: async (iconName: string) =>
+        await fetch(`${fontawesome(iconsUrl)}/brands/${iconName}.svg`).then(
+          (res) => res.text()
+        ),
+      far: async (iconName: string) =>
+        await fetch(`${fontawesome(iconsUrl)}/regular/${iconName}.svg`).then(
+          (res) => res.text()
+        ),
+      fas: async (iconName: string) =>
+        await fetch(`${fontawesome(iconsUrl)}/solid/${iconName}.svg`).then(
+          (res) => res.text()
+        ),
+      fal: async (iconName: string) =>
+        await fetch(`${fontawesome(iconsUrl)}/light/${iconName}.svg`).then(
+          (res) => res.text()
+        ),
+      fat: async (iconName: string) =>
+        await fetch(`${fontawesome(iconsUrl)}/thin/${iconName}.svg`).then(
+          (res) => res.text()
+        ),
+      fad: async (iconName: string) =>
+        await fetch(`${fontawesome(iconsUrl)}/duotone/${iconName}.svg`).then(
+          (res) => res.text()
+        ),
+      fasr: async (iconName: string) =>
+        await fetch(
+          `${fontawesome(iconsUrl)}/sharp-regular/${iconName}.svg`
+        ).then((res) => res.text()),
+      fass: async (iconName: string) =>
+        await fetch(
+          `${fontawesome(iconsUrl)}/sharp-solid/${iconName}.svg`
+        ).then((res) => res.text()),
+      fasl: async (iconName: string) =>
+        await fetch(
+          `${fontawesome(iconsUrl)}/sharp-light/${iconName}.svg`
+        ).then((res) => res.text()),
+    },
+    customizations: {
+      transform(svg, collection) {
+        if (
+          [
+            "fab",
+            "far",
+            "fas",
+            "fal",
+            "fat",
+            "fad",
+            "fasr",
+            "fass",
+            "fasl",
+          ].includes(collection)
+        )
+          return svg.replace("path", 'path fill="currentColor"');
 
-            return svg;
-          },
-        },
-      }),
-    ],
-  };
+        return svg;
+      },
+    },
+  }) as PresetFactory<any>;
+}
+
+function fontawesome(iconsUrl: string) {
+  return `${iconsUrl}/fontawesome/6/svgs`;
 }
